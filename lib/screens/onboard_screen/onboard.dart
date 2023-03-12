ee4b555b9a3354e5ecfcf428da11490478da9417
ganiular <ganiular@gmail.com>
2023-03-12 12:56:45 +0100

    center pager view ceontents

--- lib/screens/onboard_screen/onboard.dart
+++ lib/screens/onboard_screen/onboard.dart
@@ -29,98 +29,86 @@ _storeOnboardInfo() async {
 class _OnboardState extends State<Onboard> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          actions: [
-            TextButton(
-              onPressed: () async {
-                await _storeOnboardInfo();
-                Navigator.pushNamed(context, '/login');
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        actions: [
+          TextButton(
+            onPressed: () async {
+              await _storeOnboardInfo();
+              Navigator.pushNamed(context, '/login');
+            },
+            child: const Text(
+              'Skip',
+              style: TextStyle(
+                  color: Colors.deepPurpleAccent,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: PageView.builder(
+              itemCount: controller.screens.length,
+              controller: pageController,
+              onPageChanged: (int value) {
+                setState(() {
+                  currentPageIndex = value;
+                });
               },
-              child: const Text(
-                'Skip',
-                style: TextStyle(
-                    color: Colors.deepPurpleAccent,
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold),
+              itemBuilder: (_, index) {
+                return Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                  child: OnboardContent(
+                      image: controller.screens[index].imageAsset,
+                      text: controller.screens[index].text,
+                      desc: controller.screens[index].desc),
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(
+                      controller.screens.length,
+                      (index) => buildDot(index: index),
+                    ),
+                  ),
+                  const Spacer(
+                    flex: 2,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ButtonWidget(
+                    text: 'Next ',
+                    onPress: () async {
+                      if (currentPageIndex == 2) {
+                        await _storeOnboardInfo();
+                        Navigator.pushNamed(context, '/login');
+                      }
+                      pageController.nextPage(
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.bounceIn);
+                    },
+                  ),
+                  const Spacer()
+                ],
               ),
             ),
-          ],
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              flex: 2,
-              child: PageView.builder(
-                itemCount: controller.screens.length,
-                controller: pageController,
-                onPageChanged: (int value) {
-                  setState(() {
-                    currentPageIndex = value;
-                  });
-                },
-                itemBuilder: (_, index) {
-                  return Container(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 20, horizontal: 20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Expanded(
-                          child: OnboardContent(
-                              image: controller.screens[index].imageAsset,
-                              text: controller.screens[index].text,
-                              desc: controller.screens[index].desc),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: List.generate(
-                        controller.screens.length,
-                        (index) => buildDot(index: index),
-                      ),
-                    ),
-                    const Spacer(
-                      flex: 2,
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    ButtonWidget(
-                      text: 'Next ',
-                      onPress: () async {
-                        if (currentPageIndex == 2) {
-                          await _storeOnboardInfo();
-                          Navigator.pushNamed(context, '/login');
-                        }
-                        pageController.nextPage(
-                            duration: const Duration(milliseconds: 300),
-                            curve: Curves.bounceIn);
-                      },
-                    ),
-                    const Spacer()
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
