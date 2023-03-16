213ea0a96d751631256ac996627641a0a2572817
ganiular <ganiular@gmail.com>
2023-03-16 02:30:55 +0100

    firebase work again

--- lib/screens/login_screens/login_page.dart
+++ lib/screens/login_screens/login_page.dart
@@ -79,8 +79,8 @@ class _LoginPageState extends State<LoginPage> {
                           color: Colors.black54),
                     ),
                     const SizedBox(height: 8),
-                    const Row(
-                      children: [
+                    Row(
+                      children: const [
                         Expanded(
                             child: Divider(
                                 indent: 18, endIndent: 12, thickness: 1.5)),
@@ -118,8 +118,8 @@ class _LoginPageState extends State<LoginPage> {
                       ),
                     ),
                     const SizedBox(height: 12),
-                    const Row(
-                      children: [
+                    Row(
+                      children: const [
                         Expanded(
                             child: Divider(
                                 indent: 18, endIndent: 12, thickness: 1.5)),
